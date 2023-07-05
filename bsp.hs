3117d99dc7fc5a85f654d8f2d84fb4b6c10aa7ec
László Havasi <b05xf0@inf.elte.hu>
2023-07-05 17:23:45 +0200

    Update first challanges

--- bsp.hs
+++ bsp.hs
@@ -5,13 +5,16 @@ getList n = if n==0 then return [] else do i <- getLine; is <- getList (n-1); re
 
 displayPathtoPrincess :: Int -> [String] -> String
 displayPathtoPrincess n grid  = 
-  let fs = [head, last]
-      dirs = case [getCol . getRow $ grid | getRow <- fs, getCol <- fs] of
-        "p---" -> ["LEFT" , "UP"]
-        "-p--" -> ["RIGHT", "UP"]
-        "--p-" -> ["LEFT" , "DOWN"]
-        "---p" -> ["RIGHT", "DOWN"]
-  in init . unlines $ init . unlines . replicate (n `div` 2) <$> dirs
+  let selectors = [head, last]
+      dirs = case [selectCol . selectRow $ grid | selectRow <- selectors, selectCol <- selectors] of
+        'p':_       -> ["LEFT" , "UP"]
+        _:'p':_     -> ["RIGHT", "UP"]
+        _:_:'p':_   -> ["LEFT" , "DOWN"]
+        _:_:_:'p':_ -> ["RIGHT", "DOWN"]
+        _           -> []
+  in if   null dirs 
+     then "INVALID INPUT" 
+     else init . unlines $ init . unlines . replicate (n `div` 2) <$> dirs
 
 main = do
   n     <- getLine
